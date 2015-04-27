ea8ecb38a316d8a9bb443dd6104a952681eb8671
chinatszrn <chinatszrn@163.com>
2015-04-27 10:42:48 +0800

    First Pure OpenRTSP

---
+++ build/src/CMakeFiles/openRTSP.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/zhourunnan/program/openRTSP/src/openRTSP.cpp" "/Users/zhourunnan/program/openRTSP/build/src/CMakeFiles/openRTSP.dir/openRTSP.cpp.o"
+  "/Users/zhourunnan/program/openRTSP/src/playCommon.cpp" "/Users/zhourunnan/program/openRTSP/build/src/CMakeFiles/openRTSP.dir/playCommon.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src/../include"
+  "/usr/local/include"
+  "/usr/local/include/BasicUsageEnvironment"
+  "/usr/local/include/UsageEnvironment"
+  "/usr/local/include/liveMedia"
+  "/usr/local/include/groupsock"
+  "/usr/local/include/opencv"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/src/CMakeFiles/openRTSP.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/openRTSP.dir/openRTSP.cpp.o"
+  "CMakeFiles/openRTSP.dir/playCommon.cpp.o"
+  "../../bin/openRTSP.pdb"
+  "../../bin/openRTSP"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/openRTSP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
