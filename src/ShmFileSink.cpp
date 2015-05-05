3ae034bb2398bcb08a6a073226eb5349d1b61525
陈其周 <chenqizhou@chenqizhoudeMacBook-Pro.local>
2015-05-05 10:53:49 +0800

    add shm ,compile ok ,no test

--- src/ShmFileSink.cpp
+++ src/ShmFileSink.cpp
@@ -1,9 +1,70 @@
 #include "ShmFileSink.hh"
 #include "OutputFile.hh"
+
+
+
+ unsigned int ShmFileSink
+::writeBuffer(circleBuffer* circleBuf,unsigned char const* data,unsigned int length)
+ {
+   unsigned int readIndex = circleBuf->readIndex;
+  // printf("readIndex is %d\n",readIndex);
+   if (circleBuf->writeIndex >= readIndex){
+     if (circleBuf->writeIndex == readIndex && circleBuf->allowWrite == FALSE){
+       printf("Do not allowed to write!\n");
+       return -1;
+     }
+     if ((circleBuf->writeIndex + length) > BUFFER_LEN){
+       if (circleBuf->writeIndex + length - BUFFER_LEN < readIndex){
+         memcpy(circleBuf->buffer + circleBuf->writeIndex, data, BUFFER_LEN - circleBuf->writeIndex);
+         memcpy(circleBuf->buffer, data + BUFFER_LEN - circleBuf->writeIndex, circleBuf->writeIndex + length - BUFFER_LEN);
+         circleBuf->writeIndex = circleBuf->writeIndex + length - BUFFER_LEN;
+       }
+       else{
+         memcpy(circleBuf->buffer + circleBuf->writeIndex, data, BUFFER_LEN - circleBuf->writeIndex);
+         memcpy(circleBuf->buffer, data + BUFFER_LEN - readIndex, readIndex);
+         circleBuf->writeIndex = readIndex ;
+         printf("buffer full..\n");
+         circleBuf->allowWrite = FALSE;
+       }
+     }
+     else{
+       //printf("check point writeIndex %d length %d\n",circleBuf->writeIndex,length);
+       memcpy(circleBuf->buffer + circleBuf->writeIndex, data, length);
+       circleBuf->writeIndex += length;
+     }
+   }
+   else if (circleBuf->writeIndex < readIndex){
+     if (circleBuf->writeIndex + length >= readIndex){
+       memcpy(circleBuf->buffer + circleBuf->writeIndex, data, readIndex - circleBuf->writeIndex);
+       printf("buffer full..\n");
+       circleBuf->allowWrite = FALSE;
+     }
+     else{
+       memcpy(circleBuf->buffer + circleBuf->writeIndex, data, length);
+     }
+   }
+   
+   return 0;
+ }
+
+
+
 ShmFileSink
 ::ShmFileSink(UsageEnvironment& env, FILE* fid,unsigned bufferSize, char const* perFrameFileNamePrefix)
 :FileSink(env,fid,bufferSize,perFrameFileNamePrefix)
 {
+  VIDEO_SHM_ID[0]=67890;
+
+  for(int i = 0;i<VIDEO_CHANNEL_COUNT;i++)
+  {
+     shmid[i]=shmget( VIDEO_SHM_ID[i],SHARED_BUFFER_LEN,0666|IPC_CREAT );    
+          if( shmid[i]>0 )  
+              printf( "Create a shared memory segment %d\n",shmid[i] );  
+         
+          membuff[i]=shmat( shmid[i],( const void* )0,0 );  
+          //if(membuff[i] == -1) printf("shmat error!\n");
+          circleBufferArray[i] = (circleBuffer*)membuff[i];
+  }
 
 }
 
@@ -13,7 +74,10 @@ ShmFileSink:: ~ShmFileSink(){
 
 
 ShmFileSink* ShmFileSink::createNew(UsageEnvironment& env, char const* fileName,
-                  unsigned bufferSize, Boolean oneFilePerFrame) {
+                  unsigned bufferSize, Boolean oneFilePerFrame)
+{
+
+
   do {
     FILE* fid;
     char const* perFrameFileNamePrefix;
@@ -36,41 +100,41 @@ ShmFileSink* ShmFileSink::createNew(UsageEnvironment& env, char const* fileName,
 
 
 void ShmFileSink::addData(unsigned char const* data, unsigned dataSize,
-                           struct timeval presentationTime) {
- printf("hello buaa!\n");
-     if (fPerFrameFileNameBuffer != NULL && fOutFid == NULL) {
-              // Special case: Open a new file on-the-fly for this frame
-              if (presentationTime.tv_usec == fPrevPresentationTime.tv_usec &&
-                              presentationTime.tv_sec == fPrevPresentationTime.tv_sec) {
-                        // The presentation time is unchanged from the previous frame, so we add a 'counter'
-                        // suffix to the file name, to distinguish them:
-                        sprintf(fPerFrameFileNameBuffer, "%s-%lu.%06lu-%u", fPerFrameFileNamePrefix,
-                                              presentationTime.tv_sec, presentationTime.tv_usec, ++fSamePresentationTimeCounter);
-                            } else {
-                                      sprintf(fPerFrameFileNameBuffer, "%s-%lu.%06lu", fPerFrameFileNamePrefix,
-                                                            presentationTime.tv_sec, presentationTime.tv_usec);
-                                            fPrevPresentationTime = presentationTime; // for next time
-                                                  fSamePresentationTimeCounter = 0; // for next time
-                                                      }
-                  fOutFid = OpenOutputFile(envir(), fPerFrameFileNameBuffer);
-                    }
+               struct timeval presentationTime)
+ {
+  if (fPerFrameFileNameBuffer != NULL && fOutFid == NULL) {
+    // Special case: Open a new file on-the-fly for this frame
+    if (presentationTime.tv_usec == fPrevPresentationTime.tv_usec &&
+    presentationTime.tv_sec == fPrevPresentationTime.tv_sec) {
+      // The presentation time is unchanged from the previous frame, so we add a 'counter'
+      // suffix to the file name, to distinguish them:
+      sprintf(fPerFrameFileNameBuffer, "%s-%lu.%06lu-%u", fPerFrameFileNamePrefix,
+          presentationTime.tv_sec, presentationTime.tv_usec, ++fSamePresentationTimeCounter);
+    } else {
+      sprintf(fPerFrameFileNameBuffer, "%s-%lu.%06lu", fPerFrameFileNamePrefix,
+          presentationTime.tv_sec, presentationTime.tv_usec);
+      fPrevPresentationTime = presentationTime; // for next time
+      fSamePresentationTimeCounter = 0; // for next time
+    }
+    fOutFid = OpenOutputFile(envir(), fPerFrameFileNameBuffer);
+  }
 
-        // Write to our file:
+  // Write to our file:
 #ifdef TEST_LOSS
-        static unsigned const framesPerPacket = 10;
-          static unsigned const frameCount = 0;
-            static Boolean const packetIsLost;
-              if ((frameCount++)%framesPerPacket == 0) {
-                      packetIsLost = (our_random()%10 == 0); // simulate 10% packet loss #####
-                        }
+  static unsigned const framesPerPacket = 10;
+  static unsigned const frameCount = 0;
+  static Boolean const packetIsLost;
+  if ((frameCount++)%framesPerPacket == 0) {
+    packetIsLost = (our_random()%10 == 0); // simulate 10% packet loss #####
+  }
 
-                if (!packetIsLost)
+  if (!packetIsLost)
 #endif
-                    if (fOutFid != NULL && data != NULL) {
-                            fwrite(data, 1, dataSize, fOutFid);
-                              }
+  if (fOutFid != NULL && data != NULL) {
+    fwrite(data, 1, dataSize, fOutFid);
+    writeBuffer(circleBufferArray[0],  data, dataSize);
+  }
 }
-
 
 
 void ShmFileSink::afterGettingFrame(unsigned frameSize,
--- src/ShmFileSink.hh
+++ src/ShmFileSink.hh
@@ -1,15 +1,39 @@
 #ifndef _SHM_FILE_SINK_HH
 #define _SHM_FILE_SINK_HH
 
-#ifndef _FILE_SINK_HH
 #include "FileSink.hh"
-#endif
+
+
+#include <sys/shm.h>
+
+#define BUFFER_LEN  (1*1024*1024 - 12)
+#define SHARED_BUFFER_LEN (BUFFER_LEN + 12)
+#define TRUE 1
+#define FALSE 0
+#pragma pack(push) //保存对齐状态
+
+ #pragma pack(4)//设定为4字节对齐
+ typedef struct {
+  unsigned int readIndex;
+  unsigned int writeIndex;
+  unsigned int allowWrite;
+  char buffer[BUFFER_LEN];
+}circleBuffer;
+#pragma pack(pop)
+
+
+#define VIDEO_CHANNEL_COUNT 1
+
+
+
+
+
 
 class ShmFileSink: public FileSink {
     public:
           static ShmFileSink* createNew(UsageEnvironment& env, char const* fileName,unsigned bufferSize = 100000,Boolean oneFilePerFrame = False);
                 // See "FileSink.hh" for a description of these parameters.
-
+          unsigned int writeBuffer(circleBuffer* circleBuf,unsigned char const* data,unsigned int length);
           void addData(unsigned char const* data, unsigned dataSize,
                                                  struct timeval presentationTime);
           
@@ -21,6 +45,12 @@ class ShmFileSink: public FileSink {
                                                 unsigned bufferSize, char const* perFrameFileNamePrefix);
                   // called only by createNew()
               virtual ~ShmFileSink();
+    private:
+            int shmid[VIDEO_CHANNEL_COUNT],ret;  
+            circleBuffer* circleBufferArray[VIDEO_CHANNEL_COUNT];
+            void* membuff[VIDEO_CHANNEL_COUNT];
+            int VIDEO_SHM_ID[VIDEO_CHANNEL_COUNT];
+           
 };
 
 #endif
